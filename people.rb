aa7f521799aa0b6f68b0338b08c4617766ec4581
dmmanns <dmmanns555@gmail.com>
2015-10-29 14:41:33 -0400

    Created two classes for student and instructor

--- people.rb
+++ people.rb
@@ -1,31 +1,8 @@
-class Person
-	attr_accessor :name
-    def initialize (name)
-    	@name = name
-    	puts "Hi my name is #{name}"
-    end   
-end 
-    
-class Student <Person
-	def initilaize(name)
-	  @name = name
-	end	
-	
-	def learn
-      puts "I totally get it" 
-    end
+#Question 1: Created two classes; Student, Instructor
 
+class Student
 end
 
-class Instructor <Person
-  def initialize(name)
-    @name = name
-  end  
-	
-  def teach
-    puts "Everything in Ruby is an Object"  
-  end 
-
+class Instructor
 end
 
-name = Person.new("Chris" "Christina")
