287e3e6e07fe4e5ff7a69aa2b3f9bd3be310551d
dmmanns <dmmanns555@gmail.com>
2015-10-29 14:46:28 -0400

    Add teach method

--- people.rb
+++ people.rb
@@ -1,10 +1,12 @@
 #Question 1: Created two classes; Student, Instructor
 
 class Student
-  def learn 
-  puts "I get it!" 
+	def learn 
+		puts "I get it!" 
 end
 
 class Instructor
+	def teach
+		puts "Everything in Ruby is an Object."  
 end
 
