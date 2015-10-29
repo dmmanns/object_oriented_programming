181f90844f3b6ae17966ea07f5a3043be6a6f288
dmmanns <dmmanns555@gmail.com>
2015-10-29 14:56:36 -0400

    Add new instance Instructor

--- people.rb
+++ people.rb
@@ -3,15 +3,27 @@ class Person
 	def initialize(name)
 		@name = name
 	end
+	
+	def greeting
+		puts "Hi my name is #{name}"
+    end
 end
 
-class Student
+class Student <Person
 	def learn 
 		puts "I get it!" 
-end
+    end
 
-class Instructor
+class Instructor <Person
 	def teach
 		puts "Everything in Ruby is an Object."  
+    end
 end
+
+Chris = Instructor.new("Chris")
+Chris.teach
+
+
+
+
 
